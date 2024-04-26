6a36505302d1ea8d7335d3114101e8025c19ac5f
Default <samiv360@gmail.com>
2024-04-26 15:10:46 +0300

    Add Component-annotation

--- src/main/java/com/filesharing/filebin/file/filestorage/StorageProperties.java
+++ src/main/java/com/filesharing/filebin/file/filestorage/StorageProperties.java
@@ -1,7 +1,9 @@
 package com.filesharing.filebin.file.filestorage;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
