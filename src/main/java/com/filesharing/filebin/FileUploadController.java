e500592b88683ef1f25682ccebca3504d02cda7e
Default <samiv360@gmail.com>
2024-04-26 15:10:33 +0300

    Add Autowired-annotation

--- src/main/java/com/filesharing/filebin/FileUploadController.java
+++ src/main/java/com/filesharing/filebin/FileUploadController.java
@@ -1,8 +1,9 @@
 package com.filesharing.filebin;
 
-//import com.filesharing.filebin.file.FileRepositoryJdbc;
 import com.filesharing.filebin.file.database.FileJdbcDatabaseRepository;
 import com.filesharing.filebin.file.filestorage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class FileUploadController {
     private final StorageService storageService;
     private final FileJdbcDatabaseRepository fileJdbcDatabaseRepository;
 
+    @Autowired
     public FileUploadController(StorageService storageService, FileJdbcDatabaseRepository fileJdbcDatabaseRepository) {
         this.storageService = storageService;
         this.fileJdbcDatabaseRepository = fileJdbcDatabaseRepository;
