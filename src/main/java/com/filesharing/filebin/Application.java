61bf814a4444d637a54f84072ed8825b3bf130fa
Default <samiv360@gmail.com>
2024-04-29 09:55:35 +0300

    Add log entry

--- src/main/java/com/filesharing/filebin/Application.java
+++ src/main/java/com/filesharing/filebin/Application.java
@@ -1,6 +1,8 @@
 package com.filesharing.filebin;
 
 import com.filesharing.filebin.file.filestorage.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +10,11 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		logger.info("http://localhost:8080/swagger-ui.html");
 	}
 
 	@Bean
