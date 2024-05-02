1c9d0f427a7c8844a604fc5f57c20a5971415dbc
Default <samiv360@gmail.com>
2024-05-02 10:16:34 +0300

    csrf is not allowing the file uploads for some bizarre reasoin

--- src/main/java/com/filesharing/filebin/SecurityConfig.java
+++ src/main/java/com/filesharing/filebin/SecurityConfig.java
@@ -2,21 +2,22 @@ package com.filesharing.filebin;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+// https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-components
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                )
-                .csrf().disable();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf
+                        .disable()
+                );
         return http.build();
     }
+
 }
