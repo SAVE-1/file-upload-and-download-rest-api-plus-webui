68b8982ee840740be9dfeb4295a26970eef522e5
Default <samiv360@gmail.com>
2024-05-02 14:23:36 +0300

    disable csrf for development, apparently POST is an unsafe without it

--- src/main/java/com/filesharing/filebin/SecurityConfig.java
+++ src/main/java/com/filesharing/filebin/SecurityConfig.java
@@ -2,21 +2,25 @@ package com.filesharing.filebin;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-components
+// https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#deferred-csrf-token
+// The CsrfToken is needed whenever a request is made with an unsafe HTTP method, such as a POST.
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf((csrf) -> csrf
-                        .disable()
-                );
+
+        http.csrf((csrf) ->
+                csrf.disable()
+        );
         return http.build();
     }
 
