6b5319ea16b16f7935aa1c5508a02b0b7b9d0435
Default <samiv360@gmail.com>
2024-05-08 09:23:16 +0300

    move files around

--- src/main/java/com/filesharing/filebin/SecurityConfig.java
+++ src/main/java/com/filesharing/filebin/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.filesharing.filebin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-components
 // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#deferred-csrf-token
@@ -20,11 +26,32 @@ public class SecurityConfig {
 
         http.csrf((csrf) ->
                 csrf.disable()
-        ).formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
         );
+//                .formLogin(form -> form
+//                .loginPage("/login")
+//                .permitAll()
+//        );
         return http.build();
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email,password,enabled "
+                        + "from bael_users "
+                        + "where email = ?")
+                .authoritiesByUsernameQuery("select email,authority "
+                        + "from authorities "
+                        + "where email = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
