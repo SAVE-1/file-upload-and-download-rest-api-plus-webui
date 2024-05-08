12551ed6cba229b0f77c38f117187c1df1b2021c
Default <samiv360@gmail.com>
2024-05-08 14:37:41 +0300

    usings cleaning

--- src/main/java/com/filesharing/filebin/Application.java
+++ src/main/java/com/filesharing/filebin/Application.java
@@ -1,14 +1,10 @@
 package com.filesharing.filebin;
 
+import com.filesharing.filebin.controller.FileUploadController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class Application {
