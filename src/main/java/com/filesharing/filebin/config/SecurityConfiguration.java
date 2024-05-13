b195357f421d098062eeae551fc50da15890e66d
Default <samiv360@gmail.com>
2024-05-13 13:39:37 +0300

    optimize imports and delete commented code

--- src/main/java/com/filesharing/filebin/config/SecurityConfiguration.java
+++ src/main/java/com/filesharing/filebin/config/SecurityConfiguration.java
@@ -2,21 +2,11 @@ package com.filesharing.filebin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -27,28 +17,6 @@ import java.util.List;
 // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-components
 // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#deferred-csrf-token
 // The CsrfToken is needed whenever a request is made with an unsafe HTTP method, such as a POST.
-
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.csrf((csrf) ->
-//                         csrf.disable()
-//                )
-////                .authorizeHttpRequests(authorize -> authorize
-////                        .requestMatchers(HttpMethod.POST, "localhost:8080/swagger-ui.html").permitAll()
-////                )
-////                .httpBasic(Customizer.withDefaults())
-////                .formLogin(Customizer.withDefaults()
-////                )
-//                .build();
-//
-//
-//    }
-//
-//}
 
 @Configuration
 @EnableWebSecurity
@@ -97,7 +65,3 @@ public class SecurityConfiguration {
         return source;
     }
 }
-
-
-
-
