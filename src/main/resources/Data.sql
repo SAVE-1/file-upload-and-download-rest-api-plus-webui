adb2542ab02f88509cb4fb7cd329083653005202
Default <samiv360@gmail.com>
2024-05-13 13:39:10 +0300

    comment data and schemas away, but gonna keep just in case

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,2 +1,18 @@
-INSERT INTO [Filebin].[dbo].[test] (name, timestamp)
-VALUES ('!!! HELLO WORLD !!!', CURRENT_TIMESTAMP);
+--INSERT INTO [Filebin].[dbo].[test] (name, timestamp)
+--VALUES ('!!! HELLO WORLD !!!', CURRENT_TIMESTAMP);
+--
+--INSERT INTO [Filebin].[dbo].[users] (username, password, enabled)
+--VALUES ('admin', 'my_super_secret_password_1234_$%@!', 1);
+--
+--INSERT INTO [Filebin].[dbo].[users] (username, password, enabled)
+--VALUES ('user', 'password', 1);
+--
+--INSERT INTO [Filebin].[dbo].[authorities] (username, authority)
+--VALUES ('admin', 'ROLE_ADMIN');
+--
+--INSERT INTO [Filebin].[dbo].[authorities] (username, authority)
+--VALUES ('user', 'ROLE_USER');
+--
+--
+--
+--
