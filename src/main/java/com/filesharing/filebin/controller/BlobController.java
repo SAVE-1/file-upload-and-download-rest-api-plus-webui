6d51297e6ec821f6cd57473aa2bb0bb29cb8ab6d
Default <samiv360@gmail.com>
2024-06-26 12:44:45 +0300

    /writeBlobFile stopped working for some odd reason, fixed

--- src/main/java/com/filesharing/filebin/controller/BlobController.java
+++ src/main/java/com/filesharing/filebin/controller/BlobController.java
@@ -1,21 +1,12 @@
 package com.filesharing.filebin.controller;
 
 import com.azure.core.util.BinaryData;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.WritableResource;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 
-import com.azure.core.credential.*;
 import com.azure.storage.blob.*;
-import com.azure.storage.blob.models.*;
-import com.azure.storage.blob.specialized.*;
-import com.azure.storage.common.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
 
 // azurite -s -l j:\azurite -d j:\azurite\debug.log
 @RestController
@@ -23,13 +14,23 @@ import java.nio.charset.Charset;
 public class BlobController {
 
 //    @Value("azurite-blob://testcontainer/test.txt")
-//    private Resource blobFile;
 
 //    @GetMapping("/readBlobFile")
 //    public String readBlobFile() throws IOException {
-//        return StreamUtils.copyToString(
-//                this.blobFile.getInputStream(),
-//                Charset.defaultCharset());
+//        BlobClient blobClient = new BlobClientBuilder()
+//                .endpoint("http://127.0.0.1:10000")
+//                .connectionString("SharedAccessSignature=sv=2023-01-03&ss=btqf&srt=sco&st=2024-06-26T06%3A29%3A10Z&se=2031-06-27T06%3A29%3A00Z&sp=rwdxlup&sig=lmLvS69ar0Y5gkEg%2BxBRGYQuR5jnQu378nBG4WPulZc%3D;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;FileEndpoint=http://devstoreaccount1.file.core.windows.net;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;")
+//                .containerName("test1")
+//                .blobName("test1.txt")
+//                .buildClient();
+//
+//        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+//            blobClient.downloadStream(outputStream);
+//            return outputStream.toString();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return "";
+//        }
 //    }
 
     @PostMapping("/writeBlobFile")
@@ -42,7 +43,7 @@ public class BlobController {
                 .blobName("test1.txt")
                 .buildClient();
 
-        blobClient.upload(BinaryData.fromString(data));
+        blobClient.upload(BinaryData.fromString(data), true);
 
         return "file was updated";
     }
